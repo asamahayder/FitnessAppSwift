33cba34bccb1eb410cd02d68af5d66467a356171
asamahayder <asamahayder@yahoo.dk>
2020-04-13 22:32:49 +0200

    Initial Commit

---
+++ FitnessAppSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessAppSwift
+//
+//  Created by Asama Hayder on 13/04/2020.
+//  Copyright © 2020 Asama Hayder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
