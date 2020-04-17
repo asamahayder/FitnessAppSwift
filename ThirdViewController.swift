ec39c307ce53f4636bdc91d7fac43438f0acd00d
asamahayder <asamahayder@yahoo.dk>
2020-04-17 23:46:48 +0200

    created a setAnswers method inside thirdVC

--- ThirdViewController.swift
+++ ThirdViewController.swift
@@ -18,15 +18,8 @@ class ThirdViewController: UIViewController {
          self.view.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 128/255, alpha: 1)
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setAnswerObject(object: Answers){
+        answerObject = object
     }
-    */
 
 } 
