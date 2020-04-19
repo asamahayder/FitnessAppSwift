865c1c31ca3ade7c63c0b13acafefa93ff586dfb
Thaer91 <thaerthaer1234@gmail.com>
2020-04-19 15:20:58 +0200

    Database, Exercise

---
+++ FitnessAppSwift/Database.swift
@@ -0,0 +1,30 @@
+//
+//  Database.swift
+//  FitnessAppSwift
+//
+//  Created by Thaer Almalla on 19/04/2020.
+//  Copyright © 2020 Asama Hayder. All rights reserved.
+//
+
+import Foundation
+class Database {
+    
+    var exerciseList : Array<Exercise> = Array()
+    
+    
+
+    
+    func fillList()   {
+        
+        
+        
+        
+        
+        }
+    
+    init() {
+        fillList()
+        
+    }
+   
+}
---
+++ FitnessAppSwift/Exercise.swift
@@ -0,0 +1,22 @@
+//
+//  Exercise.swift
+//  FitnessAppSwift
+//
+//  Created by Thaer Almalla on 19/04/2020.
+//  Copyright © 2020 Asama Hayder. All rights reserved.
+//
+
+import Foundation
+class Exercise {
+    
+    var exerciseName: String?
+    var exerciseTime: Int?
+    var exercisePic: String?
+    var exercissDisc: String?
+    var exerciseBPart: String?
+    
+    init(exerciseName: String, exerciseTime: Int, exercisePic: String, exerciseDisc: String, exerciseBPart: String) {
+        
+    }
+    
+}
