be4fbc2145e493ab6d986e88be0ddf2daef9eeee
asamahayder <asamahayder@yahoo.dk>
2020-04-27 00:11:50 +0200

    implemented tableview for workout

--- FitnessAppSwift/FifthViewController.swift
+++ FitnessAppSwift/FifthViewController.swift
@@ -6,23 +6,31 @@
 //  Copyright © 2020 Asama Hayder. All rights reserved.
 //
 
+//some of the code regarding the tableView was inspired by the tutorial from:
+//https://blog.usejournal.com/easy-tableview-setup-tutorial-swift-4-ad48ec4cbd45
+
 import UIKit
 
-class FifthViewController: UIViewController {
+class FifthViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var stackView: UIStackView!
     var answarObject: Answers? = nil
     var database = DatabaseObject()
     lazy var exerciseList = database.getExerciseList()
     var searchQuery:[Exercise] = [Exercise]()
-    var currentTime: Int = 0
-    
     var workout: [Exercise] = [Exercise]()
+    var currentTime: Int = 0
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = UIColor.white
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.separatorColor = UIColor.green
+        return table
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         generateWorkout()
-        addToStackView()
+        createTable()
     }
     
     func setAnswerObject(object: Answers) {
@@ -59,15 +67,166 @@ class FifthViewController: UIViewController {
     }
     
     func addToStackView()  {
+        
+        //Stack View
+        let stackView   = UIStackView()
+        stackView.axis  = NSLayoutConstraint.Axis.vertical
+        stackView.distribution  = UIStackView.Distribution.equalSpacing
+        stackView.alignment = UIStackView.Alignment.center
+        stackView.spacing   = 16.0
+
         for exercise in workout{
-            let label = UILabel()
-            label.text = exercise.exerciseName
-            label.widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true
-            label.heightAnchor.constraint(equalToConstant: 50).isActive = true
-            label.textAlignment = .center
-            stackView.addArrangedSubview(label)
-            stackView.translatesAutoresizingMaskIntoConstraints = false
+            
+            //Text Label
+            let textLabel = UILabel()
+            textLabel.backgroundColor = UIColor.yellow
+            textLabel.widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true
+            textLabel.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+            textLabel.text  = exercise.exerciseName
+            textLabel.textAlignment = .center
+            
+            stackView.addArrangedSubview(textLabel)
+            
+            //let label = UILabel()
+            //label.text = exercise.exerciseName
+            //label.widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true
+            //label.heightAnchor.constraint(equalToConstant: 50).isActive = true
+            //label.textAlignment = .center
+            //stackView.addSubview(label)
+            //stackView.translatesAutoresizingMaskIntoConstraints = false
         }
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        self.view.addSubview(stackView)
     }
+    
+    func createTable() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(TableCell.self, forCellReuseIdentifier: "cellID")
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+        ])
+    }
+    
+    
+    //The following two functions and class is directly taken from the tutorial
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return workout.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as! TableCell
+        cell.backgroundColor = UIColor.white
+        cell.exerciseName.text = workout[indexPath.row].exerciseName
+        cell.exerciseTime.text = String(workout[indexPath.row].exerciseTime)
+        cell.exerciseTime.text! += " min"
+        
+        var numberOfReps = 0
+        if answarObject?.workoutType == "bodybuilding" {
+            numberOfReps = 8
+        }else{
+            numberOfReps = 6
+        }
+        cell.exerciseReps.text = String(numberOfReps)
+        cell.exerciseReps.text! += " reps"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    class TableCell: UITableViewCell {
+        
+        let cellView: UIView = {
+            let view = UIView()
+            view.backgroundColor = UIColor.orange
+            view.layer.cornerRadius = 10
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
+        
+        let exerciseName: UILabel = {
+           let label = UILabel()
+            label.text = "workoutName"
+            label.textColor = UIColor.white
+            label.font = UIFont.boldSystemFont(ofSize: 16)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        let exerciseTime: UILabel = {
+           let label = UILabel()
+            label.text = "workoutTime"
+            label.textColor = UIColor.white
+            label.font = UIFont.boldSystemFont(ofSize: 16)
+            label.textAlignment = NSTextAlignment.right
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 0
+            label.lineBreakMode = NSLineBreakMode.byWordWrapping
+            label.sizeToFit()
+            return label
+        }()
+        
+        let exerciseReps:UILabel = {
+            let label = UILabel()
+            label.text = "exerciseReps"
+            label.textColor = UIColor.white
+            label.font = UIFont.boldSystemFont(ofSize: 16)
+            label.textAlignment = NSTextAlignment.right
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 0
+            label.lineBreakMode = NSLineBreakMode.byWordWrapping
+            label.sizeToFit()
+            return label
+        }()
+        
+        
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            setupView()
+        }
+        
+        func setupView(){
+            addSubview(cellView)
+            cellView.addSubview(exerciseName)
+            cellView.addSubview(exerciseTime)
+            cellView.addSubview(exerciseReps)
+            self.selectionStyle = .none
+            
+            NSLayoutConstraint.activate([
+                cellView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+                cellView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10),
+                cellView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10),
+                cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10)
+            ])
+            
+            exerciseName.heightAnchor.constraint(equalToConstant: 200).isActive = true
+            exerciseName.widthAnchor.constraint(equalToConstant: 200).isActive = true
+            exerciseName.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+            exerciseName.leftAnchor.constraint(equalTo: cellView.leftAnchor, constant: 20).isActive = true
+            
+            exerciseTime.bottomAnchor.constraint(equalTo: cellView.bottomAnchor, constant: -5).isActive = true
+            exerciseTime.rightAnchor.constraint(equalTo: cellView.rightAnchor, constant: -10).isActive = true
+            
+            exerciseReps.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 5).isActive = true
+            exerciseReps.rightAnchor.constraint(equalTo: cellView.rightAnchor, constant: -10).isActive = true
+        }
+
+        
+        required init?(coder aDecoder: NSCoder){
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+    }
+    
     
 }
